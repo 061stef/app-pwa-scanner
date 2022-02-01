f3e6e8601b6b48df150300436b09abf9655818a3
Stefano <stafanoraparelli@gmail.com>
2022-02-01 22:17:45 +0100

    fast commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withPWA = require("next-pwa");
+
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
-    dest: "public",
+    //dest: "public",
+    sw: '/sw.js',
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === "development"
   },
+  
 });
 
