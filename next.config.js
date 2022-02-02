ac9130b5b3657791fcade12741180db849182bf9
061stef <56385444+061stef@users.noreply.github.com>
2022-02-02 12:16:29 +0100

    update node

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
-    //dest: "public",
+    dest: "public",
     sw: '/sw.js',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === "development"
+    runtimeCaching,
+		buildExcludes: [/middleware-manifest.json$/],
+    disable: process.env.NODE_ENV === 'development'
   },
   
 });
